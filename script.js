da66b9d44d77af227677e28a5e8531504b9e4a08
mosh7890 <mosh7890@gmail.com>
2017-06-04 13:28:31 +0300

    modular

--- script.js
+++ script.js
@@ -1,10 +1,91 @@
-$(function () {
+
+// var beers = [];
+
+// var sortNFlag = true;
+// var sortCFlag = true;
+// var sortRFlag = true;
+
+// var addBeer = function (name, category, rating) {
+//     beers.push({ name: name, category: category, rating: rating });
+// }
+
+// var updateBeer = function () {
+//     $(".beers-list").empty();
+//     for (i = 0; i < beers.length; i++) {
+//         $(".beers-list").append("<li>Beer Name: " + beers[i].name + ". Category: " + beers[i].category + ". Rating: " + beers[i].rating + ".");
+//     }
+// }
+
+// var sort = function (array, property, flag) {
+//     var count;
+//     while (count != array.length) {
+//         count = 1
+//         if (flag) {
+//             for (i = 0; i < array.length - 1; i++) {
+//                 if (array[i][property] > array[i + 1][property]) {
+//                     array.splice(i, 2, array[i + 1], array[i]);
+//                 }
+//                 else {
+//                     count++;
+//                 }
+//             }
+//         }
+//         else {
+//             for (i = 0; i < array.length - 1; i++) {
+//                 if (array[i][property] < array[i + 1][property]) {
+//                     array.splice(i, 2, array[i + 1], array[i]);
+//                 }
+//                 else {
+//                     count++;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// function dynamicSort(property, flag) {
+//     return function (a, b) {
+//         if (flag) {
+//             return (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+//         }
+//         else {
+//             return (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
+//         }
+//     };
+// }
+
+// $(".post-beer").click(function () {
+//     addBeer($(".beer-input").val(), $(".category-input").val(), $(".select1").val());
+//     updateBeer();
+//     $(".beer-input").val("");
+//     $(".category-input").val("");
+//     $(".select1").val("");
+// })
+
+// $(".sort-beer-name").click(function () {
+//     sort(beers, "name", sortNFlag);
+//     //beers.sort(dynamicSort('name',sortNFlag));
+//     sortNFlag = !sortNFlag
+//     updateBeer();
+// })
+
+// $(".sort-beer-category").click(function () {
+//     sort(beers, "category", sortCFlag);
+//     //beers.sort(dynamicSort('category', sortCFlag));
+//     sortCFlag = !sortCFlag
+//     updateBeer();
+// })
+
+// $(".sort-beer-rating").click(function () {
+//     sort(beers, "rating", sortRFlag);
+//     //beers.sort(dynamicSort('rating', sortRFlag));
+//     sortRFlag = !sortRFlag
+//     updateBeer();
+// })
+
+var beerReviewApp = function () {
 
     var beers = [];
-
-    var sortNFlag = true;
-    var sortCFlag = true;
-    var sortRFlag = true;
 
     var addBeer = function (name, category, rating) {
         beers.push({ name: name, category: category, rating: rating });
@@ -17,71 +98,18 @@ $(function () {
         }
     }
 
-    var sort = function (array, property, flag) {
-        var count;
-        while (count != array.length) {
-            count = 1
-            if (flag) {
-                for (i = 0; i < array.length - 1; i++) {
-                    if (array[i][property] > array[i + 1][property]) {
-                        array.splice(i, 2, array[i + 1], array[i]);
-                    }
-                    else {
-                        count++;
-                    }
-                }
-            }
-            else {
-                for (i = 0; i < array.length - 1; i++) {
-                    if (array[i][property] < array[i + 1][property]) {
-                        array.splice(i, 2, array[i + 1], array[i]);
-                    }
-                    else {
-                        count++;
-                    }
-                }
-            }
-        }
-    }
+    return {
+        addBeer: addBeer,
+        updateBeer: updateBeer
+    };
+}
 
-    function dynamicSort(property, flag) {
-        return function (a, b) {
-            if (flag) {
-                return (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-            }
-            else {
-                return (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
-            }
-        };
-    }
+var BeerReviewApp = beerReviewApp ();
 
-    $(".post-beer").click(function () {
-        addBeer($(".beer-input").val(), $(".category-input").val(), $(".select1").val());
-        updateBeer();
-        $(".beer-input").val("");
-        $(".category-input").val("");
-        $(".select1").val("");
-    })
-
-    $(".sort-beer-name").click(function () {
-        sort(beers, "name", sortNFlag);
-        //beers.sort(dynamicSort('name',sortNFlag));
-        sortNFlag = !sortNFlag
-        updateBeer();
-    })
-
-    $(".sort-beer-category").click(function () {
-        sort(beers, "category", sortCFlag);
-        //beers.sort(dynamicSort('category', sortCFlag));
-        sortCFlag = !sortCFlag
-        updateBeer();
-    })
-
-    $(".sort-beer-rating").click(function () {
-        sort(beers, "rating", sortRFlag);
-        //beers.sort(dynamicSort('rating', sortRFlag));
-        sortRFlag = !sortRFlag
-        updateBeer();
-    })
-
-});
+$(".post-beer").click(function () {
+    BeerReviewApp.addBeer($(".beer-input").val(), $(".category-input").val(), $(".select1").val());
+    BeerReviewApp.updateBeer();
+    $(".beer-input").val("");
+    $(".category-input").val("");
+    $(".select1").val("");
+})
